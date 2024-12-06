90f9d46572007f0dc20fb5b83dd7e25b8a0120ee
mregorlite <egor.infinity@gmail.com>
2024-12-06 15:51:26 +0200

    change model, fix security issues

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tp&ct@$wkoi0_tcn^=#!=rkd(yj48w=xs_%t489^%p_&oczjdl"
+SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-tp&ct@$wkoi0_tcn^=#!=rkd(yj48w=xs_%t489^%p_&oczjdl")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+]
 
 
 # Application definition
